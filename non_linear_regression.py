d68a25d757261256cb12c749c2f4237b5c11d2b5
Indervir Singh Banipal <indervir.singh.banipal@ibm.com>
2020-12-12 03:52:54 -0600

    Update project files

---
+++ non_linear_regression.py
@@ -0,0 +1,131 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(-5.0, 5.0, 0.1)
+
+##You can adjust the slope and intercept to verify the changes in the graph
+y = 2*(x) + 3
+y_noise = 2 * np.random.normal(size=x.size)
+ydata = y + y_noise
+#plt.figure(figsize=(8,6))
+plt.plot(x, ydata,  'bo')
+plt.plot(x,y, 'r')
+plt.ylabel('Dependent Variable')
+plt.xlabel('Independent Variable')
+plt.show()
+
+x = np.arange(-5.0, 5.0, 0.1)
+
+##You can adjust the slope and intercept to verify the changes in the graph
+y = 1*(x**3) + 1*(x**2) + 1*x + 3
+y_noise = 20 * np.random.normal(size=x.size)
+ydata = y + y_noise
+plt.plot(x, ydata,  'bo')
+plt.plot(x,y, 'r')
+plt.ylabel('Dependent Variable')
+plt.xlabel('Independent Variable')
+plt.show()
+
+x = np.arange(-5.0, 5.0, 0.1)
+
+##You can adjust the slope and intercept to verify the changes in the graph
+
+y = np.power(x,2)
+y_noise = 2 * np.random.normal(size=x.size)
+ydata = y + y_noise
+plt.plot(x, ydata,  'bo')
+plt.plot(x,y, 'r')
+plt.ylabel('Dependent Variable')
+plt.xlabel('Independent Variable')
+plt.show()
+
+X = np.arange(-5.0, 5.0, 0.1)
+
+##You can adjust the slope and intercept to verify the changes in the graph
+
+Y= np.exp(X)
+
+plt.plot(X,Y)
+plt.ylabel('Dependent Variable')
+plt.xlabel('Independent Variable')
+plt.show()
+
+X = np.arange(-5.0, 5.0, 0.1)
+
+Y = np.log(X)
+
+plt.plot(X,Y)
+plt.ylabel('Dependent Variable')
+plt.xlabel('Independent Variable')
+plt.show()
+
+X = np.arange(-5.0, 5.0, 0.1)
+
+
+Y = 1-4/(1+np.power(3, X-2))
+
+plt.plot(X,Y)
+plt.ylabel('Dependent Variable')
+plt.xlabel('Independent Variable')
+plt.show()
+
+import numpy as np
+import pandas as pd
+
+# downloading dataset
+#wget -nv -O china_gdp.csv https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/china_gdp.csv
+
+df = pd.read_csv("china_gdp.csv")
+df.head(10)
+
+plt.figure(figsize=(8,5))
+x_data, y_data = (df["Year"].values, df["Value"].values)
+plt.plot(x_data, y_data, 'ro')
+plt.ylabel('GDP')
+plt.xlabel('Year')
+plt.show()
+
+X = np.arange(-5.0, 5.0, 0.1)
+Y = 1.0 / (1.0 + np.exp(-X))
+
+plt.plot(X,Y)
+plt.ylabel('Dependent Variable')
+plt.xlabel('Independent Variable')
+plt.show()
+
+
+def sigmoid(x, Beta_1, Beta_2):
+    y = 1 / (1 + np.exp(-Beta_1 * (x - Beta_2)))
+    return y
+
+beta_1 = 0.10
+beta_2 = 1990.0
+
+#logistic function
+Y_pred = sigmoid(x_data, beta_1 , beta_2)
+
+#plot initial prediction against datapoints
+plt.plot(x_data, Y_pred*15000000000000.)
+plt.plot(x_data, y_data, 'ro')
+
+# Lets normalize our data
+xdata =x_data/max(x_data)
+ydata =y_data/max(y_data)
+
+from scipy.optimize import curve_fit
+popt, pcov = curve_fit(sigmoid, xdata, ydata)
+#print the final parameters
+print(" beta_1 = %f, beta_2 = %f" % (popt[0], popt[1]))
+
+x = np.linspace(1960, 2015, 55)
+x = x/max(x)
+plt.figure(figsize=(8,5))
+y = sigmoid(x, *popt)
+plt.plot(xdata, ydata, 'ro', label='data')
+plt.plot(x,y, linewidth=3.0, label='fit')
+plt.legend(loc='best')
+plt.ylabel('GDP')
+plt.xlabel('Year')
+plt.show()
+
+
